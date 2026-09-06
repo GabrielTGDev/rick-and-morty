050d485666931cc41b05fd371f49d0575c08642c
gabriel.trujillo <gabrieltrujillotb@gmail.com>
2026-09-06 13:38:05 +0100

    feat: enhance data models with relationships and casting for Character, Episode, and Location

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -1,10 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Location extends Model
 {
-    //
+    protected $guarded = [];
+
+    protected function casts(): array
+    {
+        return [
+            'raw_data' => 'array',
+        ];
+    }
+
+    /**
+     * The characters that are residents of this location.
+     */
+    public function residents(): HasMany
+    {
+        return $this->hasMany(Character::class, 'current_location_id');
+    }
 }
