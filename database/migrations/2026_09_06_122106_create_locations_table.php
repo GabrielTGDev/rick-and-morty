386c07f41a6efa62da1fe512342a9f494401e583
gabriel.trujillo <gabrieltrujillotb@gmail.com>
2026-09-06 13:30:20 +0100

    feat: add data models and migrations for characters, episodes, and locations

---
+++ database/migrations/2026_09_06_122106_create_locations_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();$table->unsignedBigInteger('external_id')->unique()->nullable(); // API ID (nullable for unknown sources)
+            $table->string('name');
+            $table->string('type')->nullable();$table->string('dimension')->nullable();
+            $table->jsonb('raw_data')->nullable(); // BONUS: Store the original JSON$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
